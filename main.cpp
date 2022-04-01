14628ae9698f3a01cffdcf4e8e1d3338cf03292c
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-04-01 21:36:16 +0000

    Pointers

--- main.cpp
+++ main.cpp
@@ -1,53 +1,32 @@
-/*8.	Write a program using structures to add two-time values
-For example:
-Enter time 1 (hh/mm/ss): 5 22 35
-Enter time 2( hh/mm/ss):3 14 24
+//Write a program to bubble sort an array using pointers
 
-Total = 8 h  36min 59sec */
-
-#include <iostream>
-#include <string.h>
+#include<iostream>  
 using namespace std;
-
-struct time
-  {
-    int hours;
-    int minutes;
-    int seconds;           
-  }t1, t2, sum;
-
-
 int main()
-  {
-    cout<<"Enter the 1st time: "<<endl;
-    cout<<"Enter 1st hour: ";
-    cin>>t1.hours;
-
-    cout<<"Enter 1st minutes: ";
-    cin>>t1.minutes;
-
-    cout<<"Enter 1st seconds: ";
-    cin>>t1.seconds;
-    cout<<endl;
-
-    cout<<"Enter the 2nd time: "<<endl;
-    cout<<"Enter 2nd hour: ";
-    cin>>t2.hours;
-
-    cout<<"Enter 2nd minutes: ";
-    cin>>t2.minutes;
-    
-    cout<<"Enter 2nd seconds: ";
-    cin>>t2.seconds;
-    cout<<endl;
-
-    sum.hours = t1.hours + t2.hours;
-    sum.minutes = t1.minutes + t2.minutes;
-    sum.seconds = t1.seconds + t2.seconds;
-
-    cout<<"The sum of time is: "<<sum.hours<<"h " 
-    <<sum.minutes<<"min "<<sum.seconds<<"sec "<<endl;
+{
+   int a[6]={ 5,6,3,8,2,1 };
+   int  n, temp=0;   
+   int *p; 
+   p = &a[0];
+   n=6;
    
-        return 0;
-    }
+    for ( int i=0; i<n; i++){
+      for( int j=0; j<n-1; j++){
+          if( *(p+j) > *(p+j+1) ){
+               temp = *(p+j);
+               *(p+j) = *(p+j+1);
+               *(p+j+1)= temp;              
+          }           
+      }  
+}
+
+cout<<"sorted array"<<endl;
+for(int i = 0; i<n; i++)  
+{
+  cout<<*(p + i);
+}
+
+  return 0;
+}
+
 
