f497aeafc9ec8cfa2637b20e33827a1f510e6d08
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-04-01 21:42:26 +0000

    Structure, add members

--- .ccls-cache/@home@runner@structures/main.cpp
+++ .ccls-cache/@home@runner@structures/main.cpp
@@ -1,24 +1,49 @@
-//Write a program to bubble sort an array using pointers
+/*For example:
+Enter time 1 (hh/mm/ss): 5 22 35
+Enter time 2( hh/mm/ss):3 14 24
 
+Total = 8 h  36min 59sec*/
 
-#include<iostream>  
+	#include <iostream>
+#include <string.h>
 using namespace std;
+
+struct time
+  {
+    	int hours;
+    	int minutes;
+   	 int seconds;           
+  	}t1, t2, sum;
+
 int main()
-{
-	int x = 5, y = 3, * p = &x, * q = &y;
-	cout << "x = " << x << ", y = " << y << '\n';
-	x = y;
-	cout << "x = " << x << ", y = " << y << '\n';
-	x = 7;
-	cout << "x = " << x << ", y = " << y << '\n';
-	*p = 10;
-	cout << "x = " << x << ", y = " << y << '\n';
-	p = q;
-	*p = 20;
-	cout << "x = " << x << ", y = " << y << '\n';
+ 	 { cout<<"Enter the 1st time: "<<endl;
+    	cout<<"Enter 1st hour: ";
+    	cin>>t1.hours;
 
+    	cout<<"Enter 1st minutes: ";
+    	cin>>t1.minutes;
 
-  return 0;
-}
+   	 cout<<"Enter 1st seconds: ";
+    	cin>>t1.seconds;
+    	cout<<endl;
 
+    	cout<<"Enter the 2nd time: "<<endl;
+    	cout<<"Enter 2nd hour: ";
+   	 cin>>t2.hours;
 
+    	cout<<"Enter 2nd minutes: ";
+    	cin>>t2.minutes;
+    
+    	cout<<"Enter 2nd seconds: ";
+   	 cin>>t2.seconds;
+   	 cout<<endl;
+
+    	sum.hours = t1.hours + t2.hours;
+   	 sum.minutes = t1.minutes + t2.minutes;
+    	sum.seconds = t1.seconds + t2.seconds;
+
+   	 cout<<"The sum of time is: "<<sum.hours<<"h " 
+    	<<sum.minutes<<"min "<<sum.seconds<<"sec "<<endl;
+   
+        return 0;
+    }
